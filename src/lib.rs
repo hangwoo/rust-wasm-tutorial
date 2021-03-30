2d97d2f98d102873415219aedcecd9a29013e3b0
bran <bran@watcha.com>
2021-03-30 22:42:53 +0900

    Update: wasm test 추가

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,17 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
+        let cells = Universe::collect_cell(width, height);
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    fn collect_cell(width: u32, height: u32) -> Vec<Cell> {
+        (0..width * height)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
@@ -55,13 +65,20 @@ impl Universe {
                     Cell::Dead
                 }
             })
-            .collect();
+            .collect()
+    }
 
-        Universe {
-            width,
-            height,
-            cells,
-        }
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn width(&self) -> u32 {
@@ -130,5 +147,21 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter() {
+            let idx = self.get_index(*row, *col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
